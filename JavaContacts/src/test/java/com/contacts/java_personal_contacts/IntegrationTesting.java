ff817192f844843641917f69ec350888437d498b
Alexander <alex.conopatscky@yandex.ru>
2020-12-13 23:26:56 +0300

    Integration testing

--- JavaContacts/src/test/java/com/contacts/java_personal_contacts/IntegrationTesting.java
+++ JavaContacts/src/test/java/com/contacts/java_personal_contacts/IntegrationTesting.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class JavaPersonalContactsApplicationTests {
+public class IntegrationTesting {
 	@Autowired
 	private MockMvc mockMvc;
 
